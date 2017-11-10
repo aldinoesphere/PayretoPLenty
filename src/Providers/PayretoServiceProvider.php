a0340f0db4066b2aa4e92d92adb4a577dee57970
Aldino Esphere <denny.sutrisno@esphere.id>
2017-11-10 13:28:48 +0700

    Just Commit

--- src/Providers/PayretoServiceProvider.php
+++ src/Providers/PayretoServiceProvider.php
@@ -23,7 +23,7 @@ use Payreto\Services\PaymentService;
 use Payreto\Methods\AccPaymentMethod;
 use Payreto\Methods\EcpPaymentMethod;
 use Payreto\Methods\DdsPaymentMethod;
-use Payreto\Methods\AecPaymentMethod;
+use Payreto\Methods\PpmPaymentMethod;
 use Payreto\Methods\AdbPaymentMethod;
 use Payreto\Methods\PdrPaymentMethod;
 use Payreto\Methods\GrpPaymentMethod;
@@ -50,7 +50,7 @@ class PayretoServiceProvider extends ServiceProvider
     	$this->registerPaymentMethod($payContainer, 'PAYRETO_ACC', AccPaymentMethod::class);
     	$this->registerPaymentMethod($payContainer, 'PAYRETO_ECP', EcpPaymentMethod::class);
     	$this->registerPaymentMethod($payContainer, 'PAYRETO_DDS', DdsPaymentMethod::class);
-    	$this->registerPaymentMethod($payContainer, 'PAYRETO_PPM', AecPaymentMethod::class);
+    	$this->registerPaymentMethod($payContainer, 'PAYRETO_PPM', PpmPaymentMethod::class);
     	$this->registerPaymentMethod($payContainer, 'PAYRETO_ADB', AdbPaymentMethod::class);
     	$this->registerPaymentMethod($payContainer, 'PAYRETO_PDR', PdrPaymentMethod::class);
     	$this->registerPaymentMethod($payContainer, 'PAYRETO_GRP', GrpPaymentMethod::class);
