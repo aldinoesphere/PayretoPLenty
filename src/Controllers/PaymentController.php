9f9e22cc35aeda1360fa433876dd145c1506cfa1
Aldino Esphere <denny.sutrisno@esphere.id>
2017-11-14 14:30:56 +0700

    Just Commit

--- src/Controllers/PaymentController.php
+++ src/Controllers/PaymentController.php
@@ -730,12 +730,13 @@ class PaymentController extends Controller
 		];
 
 		if ($paymentKey == 'PAYRETO_ECP') {
-			$parameters = [
+			array_push([
 				'amount' => $orderData->order->amounts[0]->invoiceTotal,
 				'currency' => $orderData->order->amounts[0]->currency,
 				'paymentType' => 'CP',
 				'testMode' => 'EXTERNAL'
-			];
+			], $parameters);
+			$this->getLogger(__METHOD__)->error('Payreto:parameters', $parameters);
 			$paymentConfirmation = $this->gatewayService->backOfficePayment($checkoutId, $parameters);
 		} else {
 			$paymentConfirmation = $this->gatewayService->paymentConfirmation($checkoutId, $parameters);
