f841ad162ea5f40926a75af417b15c7df58f417e
Aldino Esphere <denny.sutrisno@esphere.id>
2017-11-15 10:24:01 +0700

    Just Commit

--- src/Controllers/PaymentController.php
+++ src/Controllers/PaymentController.php
@@ -796,14 +796,14 @@ class PaymentController extends Controller
 
 	public function getItemImages($basket)
 	{
-		$imageRepository = pluginApp(\Plenty\Modules\Item\Variation\Contracts\VariationRepositoryContract::class);
+		$imageRepository = pluginApp(\Plenty\Modules\Item\VariationImage\Contracts\VariationImageRepositoryContract::class);
 		$itemImages = [];
 		foreach ($basket->basketItems as $basketItem) {
             $authHelper = pluginApp(AuthHelper::class);
             $variationId = $basketItem->variationId;
             $itemImage = $authHelper->processUnguarded(
                 function () use ($imageRepository, $variationId) {
-                    return $imageRepository->findById($variationId);
+                    return $imageRepository->findByVariationId($variationId);
                 }
             );
 			$itemImages[$basketItem->variationId] = $itemImage;
