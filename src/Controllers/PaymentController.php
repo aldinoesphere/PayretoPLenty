87e22cc771bb2afaae945bc71b0d171964485751
Aldino Esphere <denny.sutrisno@esphere.id>
2017-11-28 15:31:10 +0700

    just commit

--- src/Controllers/PaymentController.php
+++ src/Controllers/PaymentController.php
@@ -799,36 +799,29 @@ class PaymentController extends Controller
 
 	public function handleConfirmation(Twig $twig) 
 	{
-		// $orderContract = $this->orderContract;
-		// $basket = $this->getBasket();
-  //       $this->getLogger(__METHOD__)->error('Payreto:basket', $basket); 
-		// $paymentMethod = $this->paymentHelper->getPaymentMethodById($basket->methodOfPaymentId);
-		// $basketItems = $this->basketItemRepository->all();
-		// $checkoutId = $this->request->get('id');
-  //       $this->getLogger(__METHOD__)->error('Payreto:checkoutId', $checkoutId); 
-		// $paymentSettings = $this->paymentService->getPaymentSettings($paymentMethod->paymentKey);
+        $basketHelper = pluginApp(basketHelper::class);
+        $basket = $basketHelper->getBasket();
+		$paymentMethod = $this->paymentHelper->getPaymentMethodById($basket->methodOfPaymentId);
+		$checkoutId = $this->request->get('id');
+        $this->getLogger(__METHOD__)->error('Payreto:checkoutId', $checkoutId); 
+		$paymentSettings = $this->paymentService->getPaymentSettings($paymentMethod->paymentKey);
 
-		// $parameters = [
-		// 	'authentication.userId' => $this->payretoSettings['userId'],
-		// 	'authentication.password' => $this->payretoSettings['password'],
-		// 	'authentication.entityId' => $paymentSettings['entityId']
-		// ];
+		$parameters = [
+			'authentication.userId' => $this->payretoSettings['userId'],
+			'authentication.password' => $this->payretoSettings['password'],
+			'authentication.entityId' => $paymentSettings['entityId']
+		];
 
-		// $paymentServerToServer = $this->gatewayService->paymentServerToServer($checkoutId, $parameters);
-  //       $this->getLogger(__METHOD__)->error('Payreto:paymentServerToServer', $paymentServerToServer); 
-
-       
-
-		// $this->getLogger(__METHOD__)->error('Payreto:data', $data);
-		// $this->getLogger(__METHOD__)->error('Payreto:paymentMethod', $paymentMethod);
-		// $this->getLogger(__METHOD__)->error('Payreto:paymentServerToServer', $paymentServerToServer);
+		$paymentServerToServer = $this->gatewayService->paymentServerToServer($checkoutId, $parameters);
+        $this->getLogger(__METHOD__)->error('Payreto:paymentServerToServer', $paymentServerToServer); 
+        
         $paymentConfirmationData = $this->basketHelper->paymentConfirmationData();
-        // $paymentConfirmationData = array_merge($paymentConfirmationData, [
-        //     'informationUrl' => $paymentServerToServer['resultDetails']['vorvertraglicheInformationen'],
-        //     'tilgungsplan' => $paymentServerToServer['resultDetails']['tilgungsplanText'],
-        //     'checkoutId' => $paymentServerToServer['id'],
-            // 'paymentMethodName' => $paymentMethod->name
-        // ]);
+        $paymentConfirmationData = array_merge($paymentConfirmationData, [
+            'informationUrl' => $paymentServerToServer['resultDetails']['vorvertraglicheInformationen'],
+            'tilgungsplan' => $paymentServerToServer['resultDetails']['tilgungsplanText'],
+            'checkoutId' => $paymentServerToServer['id'],
+            'paymentMethodName' => $paymentMethod->name
+        ]);
         $this->getLogger(__METHOD__)->error('Payreto:paymentConfirmationData', $paymentConfirmationData);
 
         return $twig->render('Payreto::Payment.PaymentConfirmation' , $data);
