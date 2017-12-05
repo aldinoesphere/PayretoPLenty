8a508770dc725713a483f70e93e6451d5df4690a
Aldino Esphere <denny.sutrisno@esphere.id>
2017-12-05 17:46:54 +0700

    Error Message

--- src/Controllers/PaymentController.php
+++ src/Controllers/PaymentController.php
@@ -169,8 +169,8 @@ class PaymentController extends Controller
                 return $this->response->redirectTo('confirmation');
             }
 		} else {
-            $this->getLogger(__METHOD__)->alert('Payreto:error', $test);
-            // return $this->response->redirectTo('checkout');
+            // $this->getLogger(__METHOD__)->alert('Payreto:error', $test);
+            return $this->response->redirectTo('checkout');
         }
 	}
 
