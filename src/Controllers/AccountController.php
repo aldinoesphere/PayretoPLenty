f74ff66f9b32ec2a425c4aca0ad9bc5b195c492b
Aldino Esphere <denny.sutrisno@esphere.id>
2017-12-14 13:31:03 +0700

    Recurring Account

--- src/Controllers/AccountController.php
+++ src/Controllers/AccountController.php
@@ -107,7 +107,7 @@ class AccountController extends Controller
 	{
 
 		$this->getLogger(__METHOD__)->error('Payreto:accountData', $accountData);
-		$this->getLogger(__METHOD__)->error('Payreto:accountData1', $this->accountService->loadAccountByRefId($accountData['refId']);
+		$this->getLogger(__METHOD__)->error('Payreto:accountData1', $this->accountService->loadAccountByRefId($accountData['refId']));
 
 		if ($this->accountService->loadAccountByRefId($accountData['refId'])) {
 			$result = $this->saveAccounts($accountData);
