56292c06110d94e876ca5b2a79cbbfe182c5b3a2
Aldino Esphere <denny.sutrisno@esphere.id>
2017-12-18 17:05:14 +0700

    Mypayment Information

--- src/Services/PaymentService.php
+++ src/Services/PaymentService.php
@@ -213,7 +213,7 @@ class PaymentService
 		$parameters = array_merge(
 			$this->getCredentials($paymentMethod),
 			$this->getTransactionParameters($basket, $paymentMethod),
-			$this->getCustomerParameterss()
+			$this->getCustomerParameters()
 		);
 
 		// $this->paymentHelper->mapStatus();
