afa3e91fb3602ab34eedd6a46178b4ca32724848
Aldino Esphere <denny.sutrisno@esphere.id>
2017-12-19 17:07:54 +0700

    My Payment Information

--- src/Controllers/PaymentController.php
+++ src/Controllers/PaymentController.php
@@ -15,7 +15,7 @@ use Plenty\Plugin\Templates\Twig;
 
 use IO\Api\ApiResource;
 use IO\Api\ApiResponse;
-use IO\Api\ResponseCode;
+use IO\Services\NotificationService;
 
 use Payreto\Services\GatewayService;
 use Payreto\Helper\PaymentHelper;
@@ -192,7 +192,8 @@ class PaymentController extends Controller
                 return $this->response->redirectTo('confirmation');
             }
 		} else {
-            return $this->apiResponse->error(ResponseCode::OK, 'test');
+            $notification = pluginApp(NotificationService::class);
+            $notification->error('test');
         }
 	}
 
