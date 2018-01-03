d4c1df4ab3f01421189c63ee513d023aa4c11543
Aldino Said <denny.sutrisno@esphere.id>
2018-01-03 11:55:11 +0700

    * Implementation language

--- resources/js/settings.js
+++ resources/js/settings.js
@@ -2,7 +2,7 @@ function jsUcfirst(string)
 {
 	return string.charAt(0).toUpperCase() + string.slice(1);
 }
-function changeLanguage(language) {
+function payretoChangeLanguage(language) {
 	document.getElementById('payretoPaymentNameEn').style.display = 'none';
 	document.getElementById('payretoPaymentNameDe').style.display = 'none';
 	document.getElementById('payretoPaymentName'+jsUcfirst(language)).style.display = 'block';
