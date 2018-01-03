fcaca69707b988c2123ee9b01a65c9de5fbb4ddc
Aldino Said <denny.sutrisno@esphere.id>
2018-01-03 11:38:46 +0700

    * Implementation language

--- resources/js/settings.js
+++ resources/js/settings.js
@@ -1,3 +1,7 @@
+function jsUcfirst(string)
+{
+	return string.charAt(0).toUpperCase() + string.slice(1);
+}
 function changeLanguage(language) {
 	document.getElementById('payretoPaymentNameEn').style.display = 'none';
 	document.getElementById('payretoPaymentNameDe').style.display = 'none';
